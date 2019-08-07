2c413f99c6f3db08d63b1b5c5efda471cb73500d
Rob Jansen <jansen@cs.umn.edu>
2019-08-07 11:05:13 -0400

    Updated readme, added torperf results parser

--- README.md
+++ README.md
@@ -1,7 +1,59 @@
+### setup is easy with virtualenv and pip
 
-# process relay and user info
-shadowtortools stage /scratch/201901/tor/consensuses-2019-01 /scratch/201901/tor/server-descriptors-2019-01 /scratch/201901/tor/userstats-relay-country.csv -g ~/dev/tor/src/config/geoip
+    virtualenv -p /bin/python3 shadowtortoolsenv
+    source shadowtortoolsenv/bin/activate
+    pip install -r requirements.txt
 
+### read the help menus
 
-export PATH=${PATH}:~/dev/shadow-plugin-tor/build/tor/src/core/or:~/dev/shadow-plugin-tor/build/tor/src/app:~/dev/shadow-plugin-tor/build/tor/src/tools
-shadowtortools generate relayinfo_staging_2019-01-01--2019-02-01.json userinfo_staging_2019-01-01--2019-02-01.json -p network1
+    shadowtortools -h
+    shadowtortools stage -h
+    shadowtortools generate -h
+
+### grab the data we need
+
+    wget https://collector.torproject.org/archive/relay-descriptors/consensuses/consensuses-2019-01.tar.xz
+    wget https://collector.torproject.org/archive/relay-descriptors/server-descriptors/server-descriptors-2019-01.tar.xz
+    wget https://metrics.torproject.org/userstats-relay-country.csv
+    wget https://collector.torproject.org/archive/torperf/torperf-2019-01.tar.xz
+
+### extract
+
+    tar xaf consensuses-2019-01.tar.xz
+    tar xaf server-descriptors-2019-01.tar.xz
+    tar xaf torperf-2019-01.tar.xz
+
+### we also utilize privcount Tor traffic model measurements
+
+    git clone https://github.com/tmodel-ccs2018/tmodel-ccs2018.github.io.git
+
+### we also need tor
+
+    git clone https://git.torproject.org/tor.git
+    cd tor
+    ./autogen
+    ./configure --disable-asciidoc
+    make
+    cd ..
+
+### in order to generate, we need a tor and tor-gencert binaries (to generate relay keys)
+
+    export PATH=${PATH}:`pwd`/tor/src/core/or:`pwd`/tor/src/app:`pwd`/tor/src/tools
+
+### stage first, process relay and user info
+
+    shadowtortools stage consensuses-2019-01 server-descriptors-2019-01 userstats-relay-country.csv -g tor/src/config/geoip
+
+### now we can used the staged files to generate many times
+### e.g., use '-n 0.1' to generate a ShadowTor network at '10%' the scale of Tor
+
+    shadowtortools generate relayinfo_staging_2019-01-01--2019-02-01.json userinfo_staging_2019-01-01--2019-02-01.json tmodel-ccs2018.github.io -n 0.1 -p shadowtor-0.1
+
+# you can parse the torperf data so we can compare Tor and ShadowTor performance benchmarks
+
+    shadowtortools parseperf torperf-2019-01
+
+### now if you have shadow, tgen, and oniontrace installed, you can run shadow
+
+    cd shadowtor-0.1
+    shadow -w 12 shadow.config.xml > shadow.log
---
+++ shadowtortools/parse_perf.py
@@ -0,0 +1,83 @@
+import sys
+import os
+import json
+import logging
+
+from subprocess import Popen, PIPE
+
+def run(args):
+    # as long as the download is within this bytes we consider it a full one
+    tolerance = 1000
+
+    # the data we extract
+    db = {"data": {}, "type": "tgen", "version": "0.0.1"}
+
+    count, onion_count = 0, 0
+
+    # parse the files
+    for root, dirs, files in os.walk(args.torperf_data_path):
+        for file in files:
+            fullpath = os.path.join(root, file)
+            logging.info("Processing TorPerf file: '{}'".format(fullpath))
+            with open(fullpath, 'r') as f:
+                for line in f:
+                    if '=' not in line: continue
+                    d = {}
+                    parts = line.strip().split()
+                    for part in parts:
+                        if '=' not in part: continue
+                        p = part.split('=')
+                        assert len(p) > 1
+                        key, value = p[0], p[1]
+                        d[key] = value
+
+                    if 'ENDPOINTREMOTE' in d and '.onion' in d['ENDPOINTREMOTE']:
+                        onion_count += 1
+                        continue
+                    else:
+                        count += 1
+
+                    start = float(d["START"])
+                    #if start < begintime or (endtime > 0 and start > endtime): continue
+                    if int(d["DIDTIMEOUT"]) == 1: continue
+
+                    name = d['HOSTNAMELOCAL']
+                    db['data'].setdefault(name, {"measurement_ip": "unknown", "tgen": {"stream_summary" :{"errors":{}, "time_to_first_byte_recv":{}, "time_to_first_byte_send":{}, "time_to_last_byte_recv":{}, "time_to_last_byte_send":{}}}})
+
+                    totalbytes = int(d["READBYTES"])
+                    if totalbytes < 1: continue
+                    if totalbytes > 51200-tolerance and totalbytes < 51200+tolerance: totalbytes = 51200
+                    if totalbytes > 1048576-tolerance and totalbytes < 1048576+tolerance: totalbytes = 1048576
+                    if totalbytes > 5242880-tolerance and totalbytes < 5242880+tolerance: totalbytes = 5242880
+
+                    if totalbytes not in db["data"][name]["tgen"]["stream_summary"]["time_to_first_byte_recv"]:
+                        db["data"][name]["tgen"]["stream_summary"]["time_to_first_byte_recv"][totalbytes] = {1800:[]}
+                        db["data"][name]["tgen"]["stream_summary"]["time_to_last_byte_recv"][totalbytes] = {1800:[]}
+
+                    first = float(d["DATARESPONSE"])
+                    if first >= start:
+                        db["data"][name]["tgen"]["stream_summary"]["time_to_first_byte_recv"][totalbytes][1800].append(first-start)
+
+                    last = float(d["DATACOMPLETE"])
+                    if last >= start:
+                        db["data"][name]["tgen"]["stream_summary"]["time_to_last_byte_recv"][totalbytes][1800].append(last-start)
+
+    logging.info("We processed {} downloads and skipped {} onion downloads from the source data".format(count, onion_count))
+
+    # this filename is used so that `tgentools plot` will automatically work!
+    path = args.output_path
+    if args.do_compress:
+        with open("/dev/null", 'a') as nullf:
+            path = "{}.xz".format(path)
+            xzp = Popen([args.xz_path, "-"], stdin=PIPE, stdout=PIPE)
+            ddp = Popen([args.dd_path, "of={}".format(path)], stdin=xzp.stdout, stdout=nullf, stderr=nullf)
+            str = json.dumps(db, sort_keys=True, separators=(',', ': '), indent=2)
+            xzp.stdin.write(str.encode('utf-8'))
+            xzp.stdin.close()
+            xzp.wait()
+            ddp.wait()
+    else:
+        with open(path, 'w') as outf:
+            json.dump(db, outf, sort_keys=True, separators=(',', ': '), indent=2)
+
+    logging.info("Output stored in '{}'".format(path))
