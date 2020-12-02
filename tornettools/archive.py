275371f76723507dc7f8a952b1c539d10d0c3d7e
Rob Jansen <jansen@cs.umn.edu>
2020-12-01 19:46:40 -0500

    Fixups for archive mode

--- tornettools/archive.py
+++ tornettools/archive.py
@@ -8,6 +8,8 @@ import subprocess
 from tornettools.util import which
 
 def run(args):
+    logging.info("Starting to archive simulation results now.")
+
     if which('xz') == None or which('tar') == None or which('dd') == None:
         logging.warning("We require the tar, xz, and dd tools to archive the results.")
         logging.critical("Unable to archive with missing tools.")
@@ -15,25 +17,38 @@ def run(args):
 
     shutil.copy2(f"{args.prefix}/shadow.data/hosts/4uthority1/cached-consensus", f"{args.prefix}/consensus")
 
+    logging.info("Compressing consensus.")
     __xz_parallel(args, "consensus")
-    __xz_parallel(args, "dstat.log")
-    __xz_parallel(args, "free.log")
-    __xz_parallel(args, "shadow.log")
+    logging.info("Compressing shadow config.")
     __xz_parallel(args, "shadow.config.xml")
+    logging.info("Compressing dstat log.")
+    __xz_parallel(args, "dstat.log")
+    logging.info("Compressing free log.")
+    __xz_parallel(args, "free.log")
+    logging.info("Compressing shadow log.")
+    __xz_parallel(args, "shadow.log")
 
+    logging.info("Compressing conf dir.")
     if __tar_xz_parallel(args, "conf"):
         shutil.rmtree(f"{args.prefix}/conf")
 
+    logging.info("Compressing shadow template dir.")
     if __tar_xz_parallel(args, "shadow.data.template"):
         shutil.rmtree(f"{args.prefix}/shadow.data.template")
 
+    logging.info("Compressing shadow data dir.")
     if __tar_xz_parallel(args, "shadow.data", excludes=['cached-*', 'diff-cache', 'keys', 'lock']):
         shutil.rmtree(f"{args.prefix}/shadow.data")
+
+    logging.info("Compressing remaining log files.")
+    for name in os.listdir(args.prefix):
+        if name.endswith(".log"):
+            __xz_parallel(args, name)
 
 def __xz_parallel(args, filename):
     path = f"{args.prefix}/{filename}"
     if os.path.exists(path):
-        comproc = subprocess.run(shlex.split(f"xz -9e --threads={args.nprocesses} {path}"),
+        comproc = subprocess.run(shlex.split(f"xz -9 --threads={args.nprocesses} {path}"),
             cwd=args.prefix, stdout=subprocess.DEVNULL)
         if comproc.returncode == 0:
             return True
@@ -53,13 +68,13 @@ def __tar_xz_parallel(args, dirname, excludes=[]):
     # NOTE: DO NOT USE shlex.split() in the tar command;
     # we need the ' chars in the exclude strings for the tar command, but shlex removes them
     tar_cmd = f"tar cf - {flag_str} {dirname}"
-    tarproc = Popen(tar_cmd.split(), cwd=args.prefix, stdout=subprocess.PIPE)
+    tarproc = subprocess.Popen(tar_cmd.split(), cwd=args.prefix, stdout=subprocess.PIPE)
 
-    xz_cmd = f"xz -8e --threads={args.nprocesses} -"
-    xzproc = Popen(shlex.split(xz_cmd), cwd=args.prefix, stdin=tarproc.stdout, stdout=subprocess.PIPE)
+    xz_cmd = f"xz -9 --threads={args.nprocesses} -"
+    xzproc = subprocess.Popen(shlex.split(xz_cmd), cwd=args.prefix, stdin=tarproc.stdout, stdout=subprocess.PIPE)
 
     dd_cmd = f"dd of={dirname}.tar.xz"
-    ddproc = Popen(shlex.split(dd_cmd), cwd=args.prefix, stdin=xzproc.stdout, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    ddproc = subprocess.Popen(shlex.split(dd_cmd), cwd=args.prefix, stdin=xzproc.stdout, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 
     # wait for the above and collec the return codes
     tar_rc = tarproc.wait()
