403626158dbd30e234198b7996b008ef848f8885
Rob Jansen <jansen@cs.umn.edu>
2020-12-03 19:39:28 -0500

    Framework for plot command and graphs for circuit rtt

--- tornettools/plot.py
+++ tornettools/plot.py
@@ -2,11 +2,21 @@ import sys
 import os
 import logging
 
+from itertools import cycle
+import matplotlib.pyplot as pyplot
+
+from tornettools.util import load_json_data, find_matching_files_in_dir
+
+from tornettools.plot_common import *
 from tornettools.plot_tgen import plot_tgen
 from tornettools.plot_oniontrace import plot_oniontrace
 
 def run(args):
     logging.info("Plotting simulation results now")
+    set_plot_options()
+
+    logging.info("Plotting tornet comparisons")
+    __plot_tornet(args)
 
     if args.plot_all:
         logging.info("Plotting all individual simulation results is requested")
@@ -17,4 +27,111 @@ def run(args):
         logging.info("Attempting to plot individual oniontrace results")
         plot_oniontrace(args)
 
-    logging.info("Plotting tornet comparisons")
+def __plot_tornet(args):
+    torperf_dbs = __load_torperf_datasets(args.torperf)
+
+    __plot_rtt(args, torperf_dbs)
+
+def __plot_rtt(args, torperf_dbs):
+    # get the tornet rtts
+    tornet_dbs = __load_tornet_datasets(args, f"round_trip_time.json")
+
+    # cache the torperf rtts in the 'data' keyword
+    for torperf_db in torperf_dbs:
+        torperf_db['data'] = [torperf_db['dataset']['circuit_rtt']]
+
+    dbs_to_plot = torperf_dbs + tornet_dbs
+    filename = f"{args.prefix}/round_trip_time.pdf"
+
+    __plot_cdf_figure(args, dbs_to_plot, filename,
+        yscale="taillog",
+        xlabel="Circuit Round Trip Time (s)",
+        ylabel="CDF")
+
+def __plot_cdf_figure(args, dbs, filename, xscale=None, yscale=None, xlabel=None, ylabel=None):
+    color_cycle = cycle(DEFAULT_COLORS)
+    linestyle_cycle = cycle(DEFAULT_LINESTYLES)
+
+    f = pyplot.figure()
+    lines, labels = [], []
+
+    for db in dbs:
+        if len(db['data']) == 1:
+            plot_func, d = draw_cdf, db['data'][0]
+        else:
+            plot_func, d = draw_cdf_ci, db['data']
+
+        line = plot_func(pyplot, d,
+            label=db['label'],
+            color=db['color'] or next(color_cycle),
+            linestyle=next(linestyle_cycle))
+
+        lines.append(line)
+        labels.append(db['label'])
+
+    if xscale is not None:
+        pyplot.xscale(xscale)
+        if xlabel != None:
+            xlabel += __get_scale_suffix(xscale)
+    if yscale != None:
+        pyplot.yscale(yscale)
+        if ylabel != None:
+            ylabel += __get_scale_suffix(yscale)
+    if xlabel != None:
+        pyplot.xlabel(xlabel)
+    if ylabel != None:
+        pyplot.ylabel(ylabel)
+
+    m = 0.025
+    pyplot.margins(m)
+    x_visible_max = max([quantile(db['data'][0], 0.99) for db in dbs])
+    pyplot.xlim(xmin=-m*x_visible_max, xmax=(m+1)*x_visible_max)
+
+    pyplot.tick_params(axis='both', which='major', labelsize=8)
+    pyplot.tick_params(axis='both', which='minor', labelsize=5)
+    pyplot.grid(True, axis='both', which='minor', color='0.1', linestyle=':', linewidth='0.5')
+    pyplot.grid(True, axis='both', which='major', color='0.1', linestyle=':', linewidth='1.0')
+
+    pyplot.legend(lines, labels, loc='best')
+    pyplot.tight_layout(pad=0.3)
+    pyplot.savefig(filename)
+
+def __get_scale_suffix(scale):
+    if scale == 'taillog':
+        return " (tail log scale)"
+    elif scale == 'log':
+        return " (log scale)"
+    else:
+        return ""
+
+def __load_tornet_datasets(args, filename):
+    tornet_dbs = []
+
+    print(args.labels)
+    label_cycle = cycle(args.labels) if args.labels != None else None
+    color_cycle = cycle(args.colors) if args.colors != None else None
+
+    if args.tornet_collection_path != None:
+        for collection_dir in args.tornet_collection_path:
+            tornet_db = {
+                'data': [load_json_data(p) for p in find_matching_files_in_dir(collection_dir, filename)],
+                'label': next(label_cycle) if label_cycle != None else os.path.basename(collection_dir),
+                'color': next(color_cycle) if color_cycle != None else None,
+            }
+            tornet_dbs.append(tornet_db)
+
+    return tornet_dbs
+
+def __load_torperf_datasets(torperf_argset):
+    torperf_dbs = []
+
+    if torperf_argset != None:
+        for torperf_args in torperf_argset:
+            torperf_db = {
+                'dataset': load_json_data(torperf_args[0]) if torperf_args[0] != None else None,
+                'label': torperf_args[1] if torperf_args[1] != None else "Public Tor",
+                'color': torperf_args[2],
+            }
+            torperf_dbs.append(torperf_db)
+
+    return torperf_dbs
---
+++ tornettools/plot_common.py
@@ -0,0 +1,233 @@
+from matplotlib import use as mp_use
+mp_use('Agg') # for systems without X11
+import matplotlib.pyplot as pyplot
+from matplotlib.backends.backend_pdf import PdfPages
+
+from matplotlib import scale as mscale
+from matplotlib import transforms as mtransforms
+from matplotlib.ticker import FixedFormatter, FixedLocator
+from matplotlib import rcParams
+
+from scipy.stats import scoreatpercentile as score, t
+from numpy import ma, mean, median, std, log10, quantile, sqrt, linspace, var
+from numpy import array as nparray, log10 as nplog10
+
+DEFAULT_COLORS = ['C0', 'C1', 'C2', 'C3', 'C4', 'C5', 'C6', 'C7', 'C8', 'C9', 'C10', 'C11']
+DEFAULT_LINESTYLES = ['-', ':', '--', '-.']
+
+class TailLog(mscale.ScaleBase):
+    name = 'taillog'
+
+    def __init__(self, axis, **kwargs):
+        mscale.ScaleBase.__init__(self, axis)
+        self.nines = kwargs.get('nines', 2)
+
+    def get_transform(self):
+        return self.Transform(self.nines)
+
+    def set_default_locators_and_formatters(self, axis):
+        # axis.set_major_locator(FixedLocator(
+        #         nparray([1-10**(-k) for k in range(1+self.nines)])))
+        # axis.set_major_formatter(FixedFormatter(
+        #         [str(1-10**(-k)) for k in range(1+self.nines)]))
+
+        #majloc = [10**(-1*self.nines)*k for k in range(100) if k >= 90 or k % 10 == 0]
+        majloc = [0.0, 0.9, 0.99]
+        majloc = [round(k, self.nines) for k in majloc]
+        axis.set_major_locator(FixedLocator(nparray(majloc)))
+        axis.set_major_formatter(FixedFormatter([str(k) for k in majloc]))
+
+        minloc = [10**(-1*self.nines)*k for k in range(100) if k not in [0, 90, 99] and (k > 90 or k % 10 == 0)]
+        minloc = [round(k, self.nines) for k in minloc]
+        axis.set_minor_locator(FixedLocator(nparray(minloc)))
+        axis.set_minor_formatter(FixedFormatter([str(k) for k in minloc]))
+
+    def limit_range_for_scale(self, vmin, vmax, minpos):
+        return vmin, min(1 - 10**(-self.nines), vmax)
+
+    class Transform(mtransforms.Transform):
+        input_dims = 1
+        output_dims = 1
+        is_separable = True
+
+        def __init__(self, nines):
+            mtransforms.Transform.__init__(self)
+            self.nines = nines
+
+        def transform_non_affine(self, a):
+            masked = ma.masked_where(a > 1-10**(-1-self.nines), a)
+            if masked.mask.any():
+                return -ma.log10(1-a)
+            else:
+                return -nplog10(1-a)
+
+        def inverted(self):
+            return TailLog.InvertedTransform(self.nines)
+
+    class InvertedTransform(mtransforms.Transform):
+        input_dims = 1
+        output_dims = 1
+        is_separable = True
+
+        def __init__(self, nines):
+            mtransforms.Transform.__init__(self)
+            self.nines = nines
+
+        def transform_non_affine(self, a):
+            return 1. - 10**(-a)
+
+        def inverted(self):
+            return TailLog.Transform(self.nines)
+
+mscale.register_scale(TailLog)
+
+def __get_error_factor(k, confidence):
+    return t.ppf(confidence/2 + 0.5, k-1)/sqrt(k-1)
+
+# compute a cdf with confidence intervals based on the dataset and plot it on axis
+# dataset is a list of data
+# each data may be a list of values, or a list of [value, resolution] items
+def draw_cdf_ci(axis, dataset, confidence=0.95, **kwargs):
+    y = list(linspace(0, 0.99, num=1000))
+    quantile_buckets = {q:[] for q in y}
+
+    # we should have one empirical value for each simulation (ie data) for each quantile
+    for data in dataset:
+        num_items = len(data)
+        if num_items == 0:
+            continue
+
+        data.sort(key=getfirstorself)
+
+        for q in quantile_buckets:
+            val_at_q = data[int((num_items-1) * q)]
+            quantile_buckets[q].append(val_at_q)
+
+    # get the error factor from the student's t distribution
+    k = len(quantile_buckets[0])
+    z = __get_error_factor(k, confidence)
+
+    # compute the confidence intervals for each quantile
+    x, x_left, x_right = [], [], []
+    for i, q in enumerate(y):
+        bucket = quantile_buckets[q]
+
+        # bucket will be a list of items, each of which will either
+        # be the value (a number), or a list of two numbers (the
+        # value and the resolution).  If it's just a value, the
+        # correspinding resolution is 0.  Create the list of values
+        # and the list of resolutions.
+        emp_sample = [getfirstorself(item) for item in bucket]
+        resolutions = [getsecondorzero(item) for item in bucket]
+        assert len(emp_sample) == k and len(resolutions) == k
+
+        # The resolution variance is 1/12 of the sum of the squares
+        # of the resolutions
+        resolution_variance = sum([res**2 for res in resolutions])/12
+
+        m, v = mean(emp_sample), var(emp_sample)
+        s = sqrt(v + resolution_variance)
+
+        x_left_val = m-z*s
+        x_right_val = m+z*s
+
+        x.append(m)
+        x_left.append(max(0, x_left_val))
+        x_right.append(x_right_val)
+
+    # for debugging
+    #axis.plot(x_left, y, label=f"k={k}", color=colors[l%len(colors)], linestyle=linestyle)
+    #axis.plot(x_right, y, label=f"k={k}", color=colors[l%len(colors)], linestyle=linestyle)
+
+    # if we wanted a ccdf
+    #y = [1-q for q in y]
+
+    plot_line = axis.plot(x, y, **kwargs)
+
+    kwargs['alpha'] = 0.5
+    kwargs['linestyle'] = '-'
+
+    axis.fill_betweenx(y, x_left, x_right, **kwargs)
+    fill_line = axis.fill(0, 0, color=kwargs['color'], alpha=kwargs['alpha'])
+
+    return (plot_line[0], fill_line[0])
+
+# compute a cdf from the data and plot it on the axis
+# data may be a list of values, or a list of [value, resolution] items
+def draw_cdf(axis, data, **kwargs):
+    d = [getfirstorself(item) for item in data]
+    y = list(linspace(0.0, 1.0, num=1000))
+    x = quantile(d, y)
+    plot_line = axis.plot(x, y, **kwargs)
+    return plot_line[0]
+
+## helper - if the passed item is a list, return its first
+## element; otherwise, return the item itself
+def getfirstorself(item):
+    if isinstance(item, list):
+        return item[0]
+    return item
+
+## helper - if the passed item is a list, return its second
+## element; otherwise, return 0
+def getsecondorzero(item):
+    if isinstance(item, list):
+        return item[1]
+    return 0
+
+def log_stats(filename, msg, dist):
+    #from numpy import mean, median, std
+    #from scipy.stats import scoreatpercentile as score
+    b = sorted(dist)#.values()
+    b = [getfirstorself(item) for item in b]
+    with open(filename, 'a') as outf:
+        print(msg, file=outf)
+        print("min={} q1={} median={} q3={} max={} mean={} stddev={}".format(min(b), score(b, 25), median(b), score(b, 75), max(b), mean(b), std(b)), file=outf)
+
+def set_plot_options():
+    options = {
+        #'backend': 'PDF',
+        'font.size': 12,
+        'figure.figsize': (4,3),
+        'figure.dpi': 100.0,
+        'grid.color': '0.1',
+        'grid.linestyle': ':',
+        'grid.linewidth': 0.5,
+        'axes.grid' : True,
+        #'axes.grid.axis' : 'y',
+        #'axes.axisbelow': True,
+        'axes.titlesize' : 14,
+        'axes.labelsize' : 10,
+        'axes.formatter.limits': (-4,4),
+        'xtick.labelsize' : 8,
+        'ytick.labelsize' : 8,
+        'lines.linewidth' : 2.0,
+        'lines.markeredgewidth' : 0.5,
+        'lines.markersize' : 10,
+        'legend.fontsize' : 10,
+        'legend.fancybox' : False,
+        'legend.shadow' : False,
+        'legend.borderaxespad' : 0.5,
+        'legend.columnspacing' : 1.0,
+        'legend.numpoints' : 1,
+        'legend.handletextpad' : 0.25,
+        'legend.handlelength' : 2.0,
+        'legend.labelspacing' : 0.25,
+        'legend.markerscale' : 1.0,
+        # turn on the following to embedd fonts; requires latex
+        'ps.useafm' : True,
+        'pdf.use14corefonts' : True,
+        'text.usetex' : True,
+        #'text.latex.preamble': r'\boldmath',
+        'text.latex.preamble': r'\usepackage{amsmath}',
+    }
+
+    for option_key in options:
+        rcParams[option_key] = options[option_key]
+
+    if 'figure.max_num_figures' in rcParams:
+        rcParams['figure.max_num_figures'] = 50
+    if 'figure.max_open_warning' in rcParams:
+        rcParams['figure.max_open_warning'] = 50
+    if 'legend.ncol' in rcParams:
+        rcParams['legend.ncol'] = 50
