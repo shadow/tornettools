2fa5c49c1806b275c44b3944a23cda4d64b1436f
Rob Jansen <jansen@cs.umn.edu>
2020-12-09 19:57:40 -0500

    Parse rusage from shadow log file

--- tornettools/parse_rusage.py
+++ tornettools/parse_rusage.py
@@ -1,18 +1,27 @@
 import os
 import logging
 import datetime
+import re
 
 from numpy import mean
 
 from tornettools.util import open_readable_file, load_json_data, dump_json_data
 
 def parse_resource_usage_logs(args):
+    logging.info("Parsing resource usage from free log")
+    if __parse_free_rusage(args):
+        logging.info("Parsing resource usage from shadow log")
+        return __parse_shadow_rusage(args)
+    else:
+        return False
+
+def __parse_free_rusage(args):
     free_filepath = f"{args.prefix}/free.log"
     if not os.path.exists(free_filepath):
         free_filepath += ".xz"
 
     if not os.path.exists(free_filepath):
-        logging.warning(f"Unable to find resource usage data at {free_filepath}")
+        logging.warning(f"Unable to find memory usage data at {free_filepath}")
         return False
 
     rusage = {}
@@ -39,28 +48,77 @@ def parse_resource_usage_logs(args):
                 rusage.setdefault(last_ts, memd)
 
     if len(rusage) > 0:
-        outpath = f"{args.prefix}/free.json.xz"
+        outpath = f"{args.prefix}/free_rusage.json.xz"
         dump_json_data(rusage, outpath, compress=True)
         return True
     else:
-        logging.warning(f"Unable to parse resource data from {free_filepath}.")
+        logging.warning(f"Unable to parse memory usage data from {free_filepath}.")
+        return False
+
+def __parse_shadow_rusage(args):
+    shadow_filepath = f"{args.prefix}/shadow.log"
+    if not os.path.exists(shadow_filepath):
+        shadow_filepath += ".xz"
+
+    if not os.path.exists(shadow_filepath):
+        logging.warning(f"Unable to find cpu usage data at {shadow_filepath}")
+        return False
+
+    rusage = {}
+    heartbeat = re.compile("_slave_heartbeat")
+    with open_readable_file(shadow_filepath) as inf:
+        for line in inf:
+            if heartbeat.search(line) != None:
+                parts = line.strip().split()
+                if len(parts) >= 13:
+                    sim_time = float(parts[12]) # nanos e.g. 2000000000
+                    std = datetime.timedelta(microseconds=sim_time/1000.0)
+                    sim_secs = std.total_seconds()
+
+                    if sim_secs not in rusage:
+                        real_time = parts[0] # time e.g. 00:00:15.436056
+                        rt_parts = real_time.split(':')
+                        rtd = datetime.timedelta(hours=int(rt_parts[0]), minutes=int(rt_parts[1]), seconds=float(rt_parts[2]))
+
+                        rund = {keyval.split('=')[0]: keyval.split('=')[1] for keyval in parts if '=' in keyval}
+                        rund['real_time'] = rtd.total_seconds()
+
+                        rusage[sim_secs] = rund
+
+    if len(rusage) > 0:
+        outpath = f"{args.prefix}/shadow_rusage.json.xz"
+        dump_json_data(rusage, outpath, compress=True)
+        return True
+    else:
+        logging.warning(f"Unable to parse resource usage data from {shadow_filepath}.")
         return False
 
 def extract_resource_usage_plot_data(args):
-    json_path = f"{args.prefix}/free.json"
+    free_json_path = f"{args.prefix}/free_rusage.json"
 
-    if not os.path.exists(json_path):
-        json_path += ".xz"
+    if not os.path.exists(free_json_path):
+        free_json_path += ".xz"
 
-    if not os.path.exists(json_path):
-        logging.warning(f"Unable to find resource usage data at {json_path}.")
+    if not os.path.exists(free_json_path):
+        logging.warning(f"Unable to find memory resource usage data at {free_json_path}.")
         return
 
-    data = load_json_data(json_path)
-    __extract_resource_usage(args, data)
+    shadow_json_path = f"{args.prefix}/shadow_rusage.json"
 
-def __extract_resource_usage(args, data):
-    rusage = {"ram": __get_ram_usage(data), "run_time": __get_run_time(data)}
+    if not os.path.exists(shadow_json_path):
+        shadow_json_path += ".xz"
+
+    if not os.path.exists(shadow_json_path):
+        logging.warning(f"Unable to find memory resource usage data at {shadow_json_path}.")
+        return
+
+    free_data = load_json_data(free_json_path)
+    shadow_data = load_json_data(shadow_json_path)
+
+    __extract_resource_usage(args, free_data, shadow_data)
+
+def __extract_resource_usage(args, free_data, shadow_data):
+    rusage = {"ram": __get_ram_usage(free_data), "run_time": __get_run_time(shadow_data)}
     outpath = f"{args.prefix}/tornet.plot.data/resource_usage.json"
     dump_json_data(rusage, outpath, compress=False)
 
@@ -85,13 +143,12 @@ def __get_ram_usage(data):
     return {"bytes_used_max": bytes_used_max, "gib_used_max": gib_used_max, "gib_used_per_minute": gib_used_per_minute}
 
 def __get_run_time(data):
-    times = [float(k) for k in data.keys()]
-
-    dt_min = datetime.datetime.fromtimestamp(min(times))
-    dt_max = datetime.datetime.fromtimestamp(max(times))
-    runtime = dt_max - dt_min
+    real_seconds_per_sim_second = {float(sim_sec): float(data[sim_sec]["real_time"]) for sim_sec in data}
+    times = list(real_seconds_per_sim_second.values())
+    runtime = datetime.timedelta(seconds=max(times))
 
     return {"human": str(runtime),
         "seconds": runtime.total_seconds(),
         "minutes": runtime.total_seconds()/60.0,
-        "hours": runtime.total_seconds()/3600.0}
+        "hours": runtime.total_seconds()/3600.0,
+        "real_seconds_per_sim_second": real_seconds_per_sim_second}
