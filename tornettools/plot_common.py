19d8f8a154aa7b7750dad01316fbb105359e2504
Rob Jansen <jansen@cs.umn.edu>
2020-12-19 11:34:02 -0500

    Make sure overlapping datasets are visible in plots

--- tornettools/plot_common.py
+++ tornettools/plot_common.py
@@ -15,7 +15,7 @@ from numpy import array as nparray, log10 as nplog10
 from tornettools.util import which
 
 DEFAULT_COLORS = ['C0', 'C1', 'C2', 'C3', 'C4', 'C5', 'C6', 'C7', 'C8', 'C9', 'C10', 'C11']
-DEFAULT_LINESTYLES = ['-', ':', '--', '-.']
+DEFAULT_LINESTYLES = ['-', '--', ':', '-.']
 
 class TailLog(mscale.ScaleBase):
     name = 'taillog'
