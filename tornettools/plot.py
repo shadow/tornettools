587e91e568a8d6f2fb7cdb41bf82c5945d1ac4ba
Jim Newsome <jnewsome@torproject.org>
2022-02-22 16:01:34 -0600

    Don't escape % (#68)

--- tornettools/plot.py
+++ tornettools/plot.py
@@ -260,7 +260,7 @@ def __plot_transfer_error_rates(args, circuittype, torperf_dbs, tornet_dbs, erro
     dbs_to_plot = torperf_dbs + tornet_dbs
 
     __plot_cdf_figure(args, dbs_to_plot, f"transfer_error_rates_{error_key}.{circuittype}",
-                      xlabel=f"{circuittype} Transfer Error Rate (\\%): Type={error_key}")
+                      xlabel=f"{circuittype} Transfer Error Rate (%): Type={error_key}")
 
 def __plot_client_goodput(args, circuittype, torperf_dbs, tornet_dbs):
     if circuittype == 'onionservice':
